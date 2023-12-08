1452b9ef09b53c3210878e31e28855e22b59b5f5
Famuwagun Oluwatofunmi <tofunmifamu@gmail.com>
2023-12-08 08:44:23 -0800

    corrections and some extrafiles

--- _getline.c
+++ _getline.c
@@ -1,109 +1,97 @@
 #include "shell.h"
 
 /**
- * _getline - this reads input from standard input by user
- * Return: the input on a buffer
- */
-char *_getline()
+* _getline - read one line from the prompt.
+* @data: struct for the program's data
+*
+* Return: reading counting bytes.
+*/
+int _getline(data_of_program *data)
 {
-	int i, rd, buffsize = BUFSIZE;
-	char c = 0, *buffer, *buf;
+	char buff[BUFFER_SIZE] = {'\0'};
+	static char *array_commands[10] = {NULL};
+	static char array_operators[10] = {'\0'};
+	ssize_t bytes_read, i = 0;
 
-	buffer = malloc(buffsize);
-	if (buffer == NULL)
+	/* check if doesnot exist more commands in the array */
+	/* and checks the logical operators */
+	if (!array_commands[0] || (array_operators[0] == '&' && errno != 0) ||
+		(array_operators[0] == '|' && errno == 0))
 	{
-		free(buffer);
-		return (NULL);
+		/*free the memory allocated in the array if it exists */
+		for (i = 0; array_commands[i]; i++)
+		{
+			free(array_commands[i]);
+			array_commands[i] = NULL;
+		}
+
+		/* read from the file descriptor int to buff */
+		bytes_read = read(data->file_descriptor, &buff, BUFFER_SIZE - 1);
+		if (bytes_read == 0)
+			return (-1);
+
+		/* split lines for \n or ; */
+		i = 0;
+		do {
+			array_commands[i] = str_duplicate(_strtok(i ? NULL : buff, "\n;"));
+			/*checks and split for && and || operators*/
+			i = check_logic_ops(array_commands, i, array_operators);
+		} while (array_commands[i++]);
 	}
-	for (i = 0; c != EOF && c != '\n'; i++)
+
+	/*obtains the next command (command 0) and remove it for the array*/
+	data->input_line = array_commands[0];
+	for (i = 0; array_commands[i]; i++)
 	{
-		fflush(stdin);
-		rd = read(STDIN_FILENO, &c, 1);
-		if (rd == 0)
-		{
-			free(buffer);
-			exit(EXIT_SUCCESS);
-		}
-		buffer[i] = c;
-		if (buffer[0] == '\n')
-			return (enter(buffer));
-		if (i >= buffsize)
-		{
-			buffer = realloc(buffer, (buffsize + 2));
-			if (buffer == NULL)
-			{
-				free(buffer);
-				return (NULL);
-			}
-		}
+		array_commands[i] = array_commands[i + 1];
+		array_operators[i] = array_operators[i + 1];
 	}
-	buffer[i] = '\0';
-	buf = space(buffer);
-	free(buffer);
-	hashtag_handler(buf);
-	return (buf);
+
+	return (str_length(data->input_line));
 }
+
 
 /**
- * enter - This handles newline character input
- * @string: String to be handled
- * Return: Empty string
- */
-char *enter(char *string)
+* check_logic_ops - checks and split for && and || operators
+* @array_commands: the array of the commands.
+* @i: index in the array_commands to be checked
+* @array_operators: the array of the logical operators for each previous command
+*
+* Return: index of the last command in the array_commands.
+*/
+int check_logic_ops(char *array_commands[], int i, char array_operators[])
 {
-	free(string);
-	return ("\0");
-}
+	char *temp = NULL;
+	int j;
 
-/**
- * space - This modifies the input string to remove preceeding whitespaces
- * @str: Input to be modifies
- * Return: Returns the modified string
- */
-char *space(char *str)
-{
-	int i, j = 0;
-	char *buff;
-
-	buff = malloc(sizeof(char) * (_strlen(str) + 1));
-	if (buff == NULL)
+	/* checks for the & char in the command line*/
+	for (j = 0; array_commands[i] != NULL  && array_commands[i][j]; j++)
 	{
-		perror("Allocation error");
-		return (NULL);
-	}
-	for (i = 0; str[i] == ' '; i++);
-
-	for (; str[i] != '\0'; i++)
-	{
-		buff[j] = str[i];
-	if (str[i] != ' ')
-		j++;
-	else if (str[i + 1] != ' ' && str[i + 1] != '\0')
-	}
-	buff[j] = '\0';
-	if (buff[0] == '\0' || buff[0] == '#')
-	{
-		free(buff);
-		return ("\0");
-	}
-	return (buff);
-}
-
-/**
- * hashtag_handler - This is the function that removes everything after '#'
- * @buff: input buffer
- * Return: nothing
- */
-void hashtag_handler(char *buff)
-{
-	int i;
-
-	for (i = 0; buff[i] != '\0'; i++)
-	{
-		if (buff[i] == '#' && buff[i - 1] == ' ' && buff[i + 1] == ' ')
+		if (array_commands[i][j] == '&' && array_commands[i][j + 1] == '&')
 		{
-			buff[i] = '\0';
+			/* split the line when chars && was found */
+			temp = array_commands[i];
+			array_commands[i][j] = '\0';
+			array_commands[i] = str_duplicate(array_commands[i]);
+			array_commands[i + 1] = str_duplicate(temp + j + 2);
+			i++;
+			array_operators[i] = '&';
+			free(temp);
+			j = 0;
+		}
+		if (array_commands[i][j] == '|' && array_commands[i][j + 1] == '|')
+		{
+			/* split the line when chars || was found */
+			temp = array_commands[i];
+			array_commands[i][j] = '\0';
+			array_commands[i] = str_duplicate(array_commands[i]);
+			array_commands[i + 1] = str_duplicate(temp + j + 2);
+			i++;
+			array_operators[i] = '|';
+			free(temp);
+			j = 0;
 		}
 	}
+	return (i);
 }
-
+////Psybah and Tofs5086////
