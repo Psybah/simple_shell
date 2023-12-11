e6f7c6bfdbe716676924208b4cbe744bf8a1f6a6
Famuwagun Oluwatofunmi <tofunmifamu@gmail.com>
2023-12-11 09:19:07 -0800

    major additions to the main.c file

--- main.c
+++ main.c
@@ -2,17 +2,70 @@
 
 int main(int ac, char **argv){
   char *prompt = "(Myshell) $ ";
-  char *lineptr;
-  size_t n = 0; 
+  char *lineptr = NULL, *lineptr_copy = NULL;
+  size_t n = 0;
+ ssize_t nchars_read;
+const char *delim = "\n";
+int num_tokens = 0;
+char *token;
+int i;
 
   /* declaring the void variables */
-  (void)ac; (void)argv;
+  (void)ac;
 
-  printf("%s", prompt);
-  getline(&lineptr, &n, stdin);
-  printf("%s\n", lineptr);
+  /* create a loop for Myshell's prompt */
+  while (1){
+	  printf("%s", prompt);
+	  nchars_read = getline(&lineptr, &n, stdin);
+	  /* to check if the getline function failed or reached EOF or user used CTRL + D */
+	  if (nchars_read == -1){
+		  printf("Exiting the Myshell ....\n");
+		  return (-1);
+	  }
+	  
+	  /* allocate a space for a copy of the lineptr */
+	  lineptr_copy = malloc(sizeof(char) *nchars_read);
+	  if (lineptr_copy== NULL){
+		  perror("tsh: memory allocation error");
+		  return (-1);
+	  }
+	  /* copy lineptr to lineptr_copy */
+	  strcpy(lineptr_copy, lineptr);
 
+
+	  /******** split the string (lineptr) into an array of words ********/
+	  /* calculate the total number of tokens */
+	  token =strtok(lineptr, delim);
+
+	  while (token != NULL){
+		  num_tokens++;
+		  token = strtok(NULL, delim);
+	  }
+	  num_tokens++;
+
+	  /* Allocate the space to hold the array of strings */
+	  argv = malloc(sizeof(char *) * num_tokens);
+
+	  /* Store each token in the argv array */ 
+	  token = strtok(lineptr_copy, delim);
+
+	  for (i = 0; token !=NULL; i++){
+		  argv[i] = malloc(sizeof(char) * strlen(token));
+		  strcpy(argv[i], token);
+
+
+		  token = strtok(NULL, delim);
+	  }
+	  argv[i] = NULL;
+
+                  
+
+   printf("%s\n", lineptr);
+
+  /* Free up allocated memory */
   free(lineptr);
+  }
+
   return (0);
 }
 
