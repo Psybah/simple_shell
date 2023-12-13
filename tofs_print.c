6c2733d7c21af704fde8ae360e33c38b935eb329
Famuwagun Oluwatofunmi <tofunmifamu@gmail.com>
2023-12-12 18:13:28 -0800

    header file created

--- tofs_print.c
+++
@@ -1,7 +0,0 @@
-#include "Myshell.h"
-
-void tofs_print(const char *message)
-{
-	write(STDOUT_FILENO, message, strlen(message));
-}
-
--- tofsprompt.c
+++
@@ -1,6 +0,0 @@
-#include "Myshell.h"
-
-void display_prompt(void)
-{
-	tofs_print("tofs_shell$");
-} 
